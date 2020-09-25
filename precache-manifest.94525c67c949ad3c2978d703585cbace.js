0a293e427c79124d92e19570be7adb22a4a0a7a7
mahir29 <mahirhardwar@gmail.com>
2020-09-25 11:00:05 +0530

    Updates

---
+++ precache-manifest.94525c67c949ad3c2978d703585cbace.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8acc60c9b7c7c5a4dafab2ce78c2e1ea",
+    "url": "/restaurant/index.html"
+  },
+  {
+    "revision": "446d82689cb48498ca9d",
+    "url": "/restaurant/static/css/2.2f24dbdc.chunk.css"
+  },
+  {
+    "revision": "4014553e78d2d81e5c7f",
+    "url": "/restaurant/static/css/main.6b4b67af.chunk.css"
+  },
+  {
+    "revision": "446d82689cb48498ca9d",
+    "url": "/restaurant/static/js/2.bc86f8ae.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/restaurant/static/js/2.bc86f8ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4014553e78d2d81e5c7f",
+    "url": "/restaurant/static/js/main.44a90f50.chunk.js"
+  },
+  {
+    "revision": "e72e63c70e906d8d2951",
+    "url": "/restaurant/static/js/runtime-main.3ac79a1e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/restaurant/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/restaurant/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/restaurant/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/restaurant/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/restaurant/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
